3f43a56e3b5d26481d79f9d59ac4cfbda8b5fb36
shawn <shawnkengkiat@gmail.com>
2025-03-05 23:22:41 +0800

    feat: adds interfaces and magnetron node

---
+++ src/magnetron/setup.py
@@ -0,0 +1,28 @@
+from setuptools import find_packages, setup
+
+package_name = 'magnetron'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+        data_files=[
+        ('share/ament_index/resource_index/packages',
+         ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='shawnchan',
+    maintainer_email='shawnkengkiat@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'toggle_io = magnetron.toggle_io:main',
+            'servo_node = magnetron.servo_node:main',
+            'hook_control_node = magnetron.hook_control_node:main'
+        ],
+    },
+)
